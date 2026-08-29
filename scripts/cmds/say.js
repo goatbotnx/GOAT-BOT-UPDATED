8a796f2d8cb4c79fc7580012d130cc337b4f1621
negative xalman <onlygoatbot.nx@gmail.com>
2026-08-29 21:08:00 +0600

    Update say.js

--- scripts/cmds/say.js
+++ scripts/cmds/say.js
@@ -10,7 +10,7 @@ module.exports = {
 		countDown: 5,
 		role: 0,
 		shortDescription: "Reply supported TTS",
-		category: "FUN & SOCIAL"
+		category: "TTS"
 	},
 
 	onStart: async function ({ message, args, event }) {
