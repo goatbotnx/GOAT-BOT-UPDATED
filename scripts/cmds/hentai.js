2fae19605b8bd1aa4b7af4d75f923458fc815f82
negative xalman <onlygoatbot.nx@gmail.com>
2026-09-07 18:47:43 +0600

    Update hentai.js

--- scripts/cmds/hentai.js
+++ scripts/cmds/hentai.js
@@ -7,7 +7,7 @@ module.exports = {
     role: 0,
     shortDescription: "Random hentai image",
     longDescription: "Get hentai image from API",
-    category: "NSFW",
+    category: "ANIME & MEDIA",
     guide: "{pn}"
   },
 
